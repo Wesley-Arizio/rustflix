4b9bf0cd343d3899bb9ebe527764dc6a2e052329
Wesley-Arizio <wesleyarizio26@gmail.com>
2023-11-29 19:04:11 -0300

    initial setup for auth service with grpc server basic implementation

--- auth/src/main.rs
+++ auth/src/main.rs
@@ -7,17 +7,52 @@ use clap::Parser;
 
 mod auth;
 
+use mongodb::{bson::doc, options::ClientOptions, Client, Collection};
+use serde::{Deserialize, Serialize};
+
 #[derive(Parser, Debug)]
 struct Cli {
     /// grpc port for Auth server
     #[arg(short, env)]
     grpc_port: String,
+
+    /// database username
+    #[arg(env)]
+    mongodb_username: String,
+
+    /// database password
+    #[arg(env)]
+    mongodb_password: String,
+
+    /// database host
+    #[arg(env)]
+    mongodb_host: String,
+
+    #[arg(env)]
+    mongodb_port: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Account {
+    id: String,
+    email: String,
+    password: String,
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<(), String> {
     dotenv::dotenv().unwrap();
     let args = Cli::parse();
+
+    let mongodb_url = format!(
+        "mongodb://{}:{}@{}:{}",
+        args.mongodb_username, args.mongodb_password, args.mongodb_host, args.mongodb_port
+    );
+
+    let mut client_options = ClientOptions::parse(&mongodb_url)
+        .await
+        .map_err(|e| e.to_string())?;
+
     let address = args
         .grpc_port
         .parse()
@@ -27,7 +62,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Server::builder()
         .add_service(AuthServer::new(auth_service))
         .serve(address)
-        .await?;
+        .await
+        .map_err(|e| e.to_string())?;
 
     Ok(())
 }
