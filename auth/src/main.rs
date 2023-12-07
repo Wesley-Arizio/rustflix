e3c0e4b3d1ce881db62fa9ec92c3b63bf24a9e57
Wesley-Arizio <wesleyarizio26@gmail.com>
2023-12-06 23:22:04 -0300

    handle error using anyhow and integrate new repository

--- auth/src/main.rs
+++ auth/src/main.rs
@@ -6,10 +6,11 @@ use auth::AuthService;
 use clap::Parser;
 
 mod auth;
+mod database;
 
-use mongodb::{bson::doc, options::ClientOptions, Client, Collection};
-use serde::{Deserialize, Serialize};
+use database::account_repository::AccountRepository;
 
+use mongodb::{options::ClientOptions, Client};
 #[derive(Parser, Debug)]
 struct Cli {
     /// grpc port for Auth server
@@ -32,15 +33,10 @@ struct Cli {
     mongodb_port: String,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-struct Account {
-    id: String,
-    email: String,
-    password: String,
-}
+use anyhow::{Context, Result};
 
 #[tokio::main]
-async fn main() -> Result<(), String> {
+async fn main() -> Result<()> {
     dotenv::dotenv().unwrap();
     let args = Cli::parse();
 
@@ -49,21 +45,25 @@ async fn main() -> Result<(), String> {
         args.mongodb_username, args.mongodb_password, args.mongodb_host, args.mongodb_port
     );
 
-    let mut client_options = ClientOptions::parse(&mongodb_url)
+    let client_options = ClientOptions::parse(&mongodb_url)
         .await
-        .map_err(|e| e.to_string())?;
+        .context("error parsing client options for mongod bclient")?;
+
+    let client = Client::with_options(client_options).context("configuring mongodb client")?;
+    let collection = client.database("auth").collection("credentials");
+    let account_repository = AccountRepository::new(collection);
+    let auth_service = AuthService::new(account_repository);
 
     let address = args
         .grpc_port
         .parse()
-        .expect("Could not parse auth gprc_port to a SocketAddr");
-    let auth_service = AuthService::default();
+        .expect("Could not parse auth gprc_port to a ScoketAddr");
 
     Server::builder()
         .add_service(AuthServer::new(auth_service))
         .serve(address)
         .await
-        .map_err(|e| e.to_string())?;
+        .context("initializing gRPC server")?;
 
     Ok(())
 }
