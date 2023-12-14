f7c0269c26b1e1979f97409fd23499d7b704c316
Wesley-Arizio <wesleyarizio26@gmail.com>
2023-12-14 15:13:00 -0300

    cargo fmt

--- auth/src/main.rs
+++ auth/src/main.rs
@@ -21,7 +21,7 @@ struct Cli {
     mongodb_url: String,
 
     #[arg(env = "AUTH_DATABASE_NAME")]
-    database_name: String
+    database_name: String,
 }
 
 #[tokio::main]
@@ -39,7 +39,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .expect("error parsing client options");
 
     let client = Client::with_options(client_options).expect("error initializing mongodb client");
-    let collection = client.database(&args.database_name).collection("credentials");
+    let collection = client
+        .database(&args.database_name)
+        .collection("credentials");
     let account_repository = AccountRepository::new(collection);
     let auth_service = AuthService::new(account_repository);
 
