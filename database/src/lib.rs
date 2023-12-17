02a31ea2be7c375616feeb75921a7b7b3a738b44
Wesley-Arizio <wesleyarizio26@gmail.com>
2023-12-17 00:31:10 -0300

    export sqlx types

--- database/src/lib.rs
+++ database/src/lib.rs
@@ -1,2 +1,13 @@
 pub mod entities;
 pub mod traits;
+
+pub mod types {
+    pub use sqlx::types::{
+        chrono::{DateTime, Utc},
+        Uuid,
+    };
+}
+
+pub mod connection {
+    pub use sqlx::{PgPool, Pool, Postgres};
+}
