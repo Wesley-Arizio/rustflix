aff4b015ca505861eff75dee1d231abf1d09bded
Wesley-Arizio <wesleyarizio26@gmail.com>
2023-12-29 22:53:18 -0300

    test: mongodb repository tests

--- auth/src/database.rs
+++ auth/src/database.rs
@@ -1,3 +1,16 @@
+use mongodb::options::ClientOptions;
+use mongodb::Client;
+
 pub mod account_repository;
 pub mod entities;
 pub mod traits;
+
+pub async fn database_setup(uri: &str, database_name: &str) -> mongodb::Database {
+    let client_options = ClientOptions::parse(uri)
+        .await
+        .expect("error parsing client options");
+
+    let client = Client::with_options(client_options).expect("error initializing mongodb client");
+
+    client.database(database_name)
+}
